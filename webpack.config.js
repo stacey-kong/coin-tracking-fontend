bae8ca7f6fb0cdf00fe39ce985dfe38e22296591
stacey <727@stlnd.net>
2021-02-10 17:45:40 +0800

    Add tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,47 @@
-var path = require("path");
+const path = require("path");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer"); // help tailwindcss to work
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+module.exports = (env, argv) => {
+  return {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".json"],
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'babel-loader'.
-      { test: /\.tsx?$/, loader: "babel-loader" },
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'babel-loader'.
+        { test: /\.tsx?$/, loader: "babel-loader" },
 
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                config: {
+                  path: path.resolve(__dirname, "./postcss.config.js"),
+                },
+                sourceMap: true,
+              },
+            },
+          ],
+        },
 
   // externals: {
   //   react: "React",
@@ -40,4 +54,5 @@ module.exports = {
     compress: true,
     port: 9000,
   },
+};
 };
