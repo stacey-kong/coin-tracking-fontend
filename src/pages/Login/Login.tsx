eabbac6b18de926584b302682c796280818fa298
stacey <727@stlnd.net>
2021-02-16 11:48:49 +0800

    Add a login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,28 @@
-
+import Layout from "../../components/Layout/Layout";
 
 export default function Login() {
   return (
     <>
-
+      <Layout>
+        <form className="m-auto mt-10 w-1/4 p-5 bg-gray-100 flex-box border-solid border-4 border-indigo-300">
+          <div className="m-auto w-3/4">
+            <label className="w-full">Username</label>
+            <input className="w-full" type="text" />
+          </div>
+          <div className="m-auto w-3/4">
+            <label className="w-full">Password</label>
+            <input className="w-full" type="password" />
+          </div>
+          <div className="m-auto mt-2 max-w-max">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </Layout>
     </>
   );
 }
