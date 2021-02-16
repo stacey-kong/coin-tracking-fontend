b3973ff51a513698cb03b40948913a5ae4b21076
stacey <727@stlnd.net>
2021-02-16 18:13:23 +0800

    Add table template on dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
+import Table from "../../utils/Table";
 
 export default function Dashboard() {
   return (
     <>
       <Header />
+      <Table />
       <Banner />
     </>
   );
