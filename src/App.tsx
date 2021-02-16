8acf8fe8129c1b9ba8235f8e14c571a788cad260
stacey <727@stlnd.net>
2021-02-16 16:21:50 +0800

    add local api and asyn function to check authentication before accessing private page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Login from "./pages/Login/Login";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login, Credentials } from "./pages/Login/Login";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 export default function App() {
+  const [token, setToken] = useState<Credentials | null>(null);
+  const handleSubmit = function (token: Credentials): void {
+    setToken(token);
+  };
+
+  if (!token) {
+    return <Login onclick={handleSubmit} />;
+  }
+
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="dashboard" />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+        {/* <Route path="/">
             <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+          </Route> */}
+      </Switch>
+    </Router>
   );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,60 @@
+import { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 
-export default function Login() {
+export interface Credentials {
+  username: string;
+  password: string;
+}
+interface LoginProps {
+  onclick: (arg0: Credentials) => void;
+}
+
+async function loginUser(Props: Credentials) {
+  return fetch("http://localhost:9010/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Props),
+  }).then((data) => data.json());
+}
+
+export function Login(Props: LoginProps) {
+  const [username, setUserName] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const setToken = Props.onclick;
+  const handleSubmit = async (e: React.FormEvent)=> {
+    e.preventDefault();
+    if (!username) {
+      alert("please input username");
+      return;
+    } else if (!password) {
+      alert("please input passward");
+      return;
+    }
+
+    const token = await loginUser({ username: username, password: password });
+    setToken(token);
+  };
   return (
     <>
       <Layout>
-        <form className="m-auto mt-10 w-1/4 p-5 bg-gray-100 flex-box border-solid border-4 border-indigo-300">
+        <form onSubmit = {handleSubmit} className="m-auto mt-10 w-1/4 p-5 bg-gray-100 flex-box border-solid border-4 border-indigo-300">
           <div className="m-auto w-3/4">
             <label className="w-full">Username</label>
-            <input className="w-full" type="text" />
+            <input
+              className="w-full"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+            />
           </div>
           <div className="m-auto w-3/4">
             <label className="w-full">Password</label>
-            <input className="w-full" type="password" />
+            <input
+              className="w-full"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="m-auto mt-2 max-w-max">
             <button
