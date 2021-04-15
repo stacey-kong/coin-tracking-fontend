4b38cc541cd516712214580b3d10fcc9ea31c12d
stacey <727@stlnd.net>
2021-04-15 15:55:46 +0800

    add package import svg into project

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,17 @@ module.exports = (env, argv) => {
             },
           ],
         },
+        {
+          test: /\.(png|jp(e*)g|svg|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "images/[hash]-[name].[ext]",
+              },
+            },
+          ],
+        },
       ],
     },
     plugins: [
